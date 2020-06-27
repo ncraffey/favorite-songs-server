49de3304127ae2404c3f2b685a66ec53a7a3ba00
ncraffey <craffey.n@husky.neu.edu>
2020-06-27 18:53:35 -0400

    idk

--- auth-server/app.js
+++ auth-server/app.js
@@ -104,13 +104,13 @@ app.get('/callback', function(req, res) {
         });
 
         // we can also pass the token to the browser to make requests from there
-        res.redirect('http://localhost:3000/#' +
+        res.redirect('https://young-reef-05613.herokuapp.com/#' +
           querystring.stringify({
             access_token: access_token,
             refresh_token: refresh_token
           }));
       } else {
-        res.redirect('http://localhost:3000/#' +
+        res.redirect('https://young-reef-05613.herokuapp.com/#' +
           querystring.stringify({
             error: 'invalid_token'
           }));
